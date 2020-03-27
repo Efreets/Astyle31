f6e624e5de70b39ab6573e310a75218f0b56a2ce
Efreet <Efreet@AVALON.local>
2020-03-27 13:01:59 +0800

    Update CMakeLists.txt, LICENSE.md, and 250 more files...

---
+++ Build/CMakeFiles/astyle.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/AStyle_3.1_windows/AStyle/src/ASBeautifier.cpp" "D:/AStyle_3.1_windows/Build/CMakeFiles/astyle.dir/src/ASBeautifier.cpp.obj"
+  "D:/AStyle_3.1_windows/AStyle/src/ASEnhancer.cpp" "D:/AStyle_3.1_windows/Build/CMakeFiles/astyle.dir/src/ASEnhancer.cpp.obj"
+  "D:/AStyle_3.1_windows/AStyle/src/ASFormatter.cpp" "D:/AStyle_3.1_windows/Build/CMakeFiles/astyle.dir/src/ASFormatter.cpp.obj"
+  "D:/AStyle_3.1_windows/AStyle/src/ASLocalizer.cpp" "D:/AStyle_3.1_windows/Build/CMakeFiles/astyle.dir/src/ASLocalizer.cpp.obj"
+  "D:/AStyle_3.1_windows/AStyle/src/ASResource.cpp" "D:/AStyle_3.1_windows/Build/CMakeFiles/astyle.dir/src/ASResource.cpp.obj"
+  "D:/AStyle_3.1_windows/AStyle/src/astyle_main.cpp" "D:/AStyle_3.1_windows/Build/CMakeFiles/astyle.dir/src/astyle_main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
